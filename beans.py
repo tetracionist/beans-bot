168d9266f1e98538a44323918c6b0db1294c4610
Alexander Wornast <40890820+tetracionist@users.noreply.github.com>
2022-12-21 22:59:16 +0000

    Added voice commands with help from ChatGPT :)

--- beans.py
+++ beans.py
@@ -6,9 +6,72 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
+
+
 @client.event
 async def on_ready():
    logs.logger.info("{0.user} is now online".format(client))
+
+@client.event
+async def on_voice_state_update(member, before, after):
+   
+    #print(f"member: {member}, before.channel:{before.channel}, after.channel: {after.channel}, before.id: {member.id}")
+
+    
+
+    # check that the id is not beans bot
+    if member.id == 1054876494222078012:
+        return
+
+    audio_source = discord.FFmpegPCMAudio('beans.mp3')
+
+    # check for existing connection 
+    if not client.voice_clients and after.channel != None:
+        # create a voice client by connecting to user's channel
+        channel = await client.fetch_channel(after.channel.id)
+        await channel.connect()
+        voice_client = discord.utils.get(client.voice_clients)
+        logs.logger.info("{0.user} created a new voice client".format(client))
+        voice_client.play(audio_source, after=None)
+
+    else:
+        # check if user has switched channels
+        if before.channel != after.channel and after.channel:
+            # find the voice client that is connected to the channel the user has switched to
+            next_channel = await client.fetch_channel(after.channel.id)
+            for vc in client.voice_clients:
+                if vc.channel == next_channel:
+                    voice_client = vc
+                    break
+            else:
+                # if no voice client was found, use the first voice client in the list
+                voice_client = client.voice_clients[0]
+
+            logs.logger.info(f"channel switch on {voice_client}")
+            # move the voice client to the new channel
+            await voice_client.move_to(next_channel)
+
+            # play the audio file
+            if voice_client.is_playing():
+                voice_client.stop()
+            voice_client.play(audio_source, after=None)
+        
+        elif after.channel == None:
+            previous_channel = await client.fetch_channel(before.channel.id)
+            for vc in client.voice_clients:
+                if vc.channel == previous_channel:
+                    voice_client = vc
+                    break
+            else:
+                # if no voice client was found, use the first voice client in the list
+                voice_client = client.voice_clients[0]
+            await voice_client.disconnect()
+
+    # Clean up the audio source after it has finished playing
+    #audio_source.cleanup()
+
+
+    
 
 @client.event
 async def on_message(message):
