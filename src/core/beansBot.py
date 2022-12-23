0603f0c1d7cb7b6e728a4dc36cea2cec58bb83ad
Alexander Wornast <40890820+tetracionist@users.noreply.github.com>
2022-12-23 20:47:01 +0000

    Added initials classes

--- src/core/beansBot.py
+++ src/core/beansBot.py
@@ -1,13 +1,56 @@
-import discord 
+import discord, core.alexisms, random as rand
+
 class beansBot():
-    def __init__(self, token):
+    def __init__(self: object, token: str, intents: object, commands: dict, prefix='!bb'):
         self.token = token
-        self.client = discord.Client()    
+        self.intents = intents
+        self.client = discord.Client(intents=intents)
+        self.prefix = prefix
 
-    async def start(self):
-
-        self.client.run(self.token)
-
+        # define commands 
+        self.commands = commands
+            
+    
+    def start(self):
         @self.client.event
         async def on_ready():
-            print(f"{self.client.user.name} is connected")
+            print("ready")
+        
+        
+        @self.client.event
+        async def on_message(message):
+
+            print(message)
+
+            # check if the message is a command
+            if message.content.startswith(self.prefix):
+                print("commad")
+
+            # if message is not a command then check if it matches a trigger word in alexisms
+            else:
+                # to prevent the bot from triggering itself if it finds a response in 
+                if message.author == self.client.user:
+                    return
+
+                # in a message someone might say multiple trigger words: e.g. Alex and shit 
+                # identify all trigger words that exist in a message 
+                trigger_words = [key for key in core.alexisms.phrases.keys() if key in message.content.lower()]  
+
+                for trigger_word in trigger_words: 
+                    responses = core.alexisms.phrases[trigger_word]
+                    if isinstance(responses, list):
+                        await message.channel.send(rand.choice(responses)) 
+
+                    elif isinstance(responses, str):
+                        await message.channel.send(responses) 
+
+                    else:
+                        return
+        
+        self.client.run(self.token) 
+
+
+
+
+
+
--- src/main.py
+++ src/main.py
@@ -1,3 +1,16 @@
-from src.core.beansBot import beansBot
+from core.beansBot import beansBot
+from discord import Intents
+from core import logs, alexisms
+from dotenv import load_dotenv
+import os, random
 
-bot = beansBot()
+
+if __name__ == '__main__':
+    load_dotenv()
+    token = os.getenv('TOKEN')
+
+    intents = Intents.default()
+    intents.message_content = True
+
+    bot = beansBot(token, intents)
+    bot.start()
